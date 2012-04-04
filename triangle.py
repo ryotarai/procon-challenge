a2d9eb89026a2a5f6b36b1d8e84e1671cd3eb162
Ryota Arai <ryota.arai@gmail.com>
2012-04-04 14:39:11 +0900

    Add triange(p.21)

---
+++ triangle.py
@@ -0,0 +1,14 @@
+def max_length(sides):
+    res = 0
+    sides.reverse()
+    len_sides = len(sides)
+    for i in xrange(len_sides):
+        for j in xrange(i + 1, len_sides):
+            for k in xrange(j + 1, len_sides):
+                if sides[i] < sides[j] + sides[k]:
+                    return sides[i] + sides[j] + sides[k]
+    return 0
+
+if __name__ == '__main__':
+    assert(max_length([2, 3, 4, 5, 10]) == 12)
+    assert(max_length([4, 5, 10, 20]) == 0)
