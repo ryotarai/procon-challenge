4fd2548305cce5a8a4e4292acdee16f01e78dd8a
Ryota Arai <ryota.arai@gmail.com>
2013-04-18 17:07:29 +0900

    Use dynamic programming.

--- 52_knapsack.rb
+++ 52_knapsack.rb
@@ -5,18 +5,27 @@ GOODS = [
   {weight: 2, value: 2},
 ]
 W = 5
+$dp = []
 
 # i番目以降の品物から重さの総和がj以下になるように選ぶ
 def choose(i, j)
-  if i == GOODS.size
-    return 0
-  elsif j < GOODS[i][:weight]
-    # この品物は入らない
-    return choose(i + 1, j)
+  if $dp[i] && $dp[i][j]
+    return $dp[i][j]
   end
 
-  # 入れない場合と入れる場合を両方試す
-  return [choose(i + 1, j), choose(i + 1, j - GOODS[i][:weight]) + GOODS[i][:value]].max
+  res = nil
+  if i == GOODS.size
+    res = 0
+  elsif j < GOODS[i][:weight]
+    # この品物は入らない
+    res = choose(i + 1, j)
+  else
+    # 入れない場合と入れる場合を両方試す
+    res = [choose(i + 1, j), choose(i + 1, j - GOODS[i][:weight]) + GOODS[i][:value]].max
+  end
+
+  $dp[i] = [] unless $dp[i]
+  return $dp[i][j] = res
 end
 
 puts choose(0, W) == 7
